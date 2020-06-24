3d1f73334d66271a52e85058fe4d39069f3ea832
Carlos Checa <carloschecaa@gmail.com>
2020-06-24 04:27:34 +0200

    First commit

--- src/components/DrawCanvas.vue
+++ src/components/DrawCanvas.vue
@@ -1,17 +1,17 @@
 <template lang="pug">
-  div.hello
-    h1 {{ msg }}
+  div.column Cosa de dibujar
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
 
 @Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+export default class DrawCanvas extends Vue {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="sass">
+  @import ../styles/variables
+  div
+    background-color: $primary-colour2
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  div#main
+    div.columns
+      div.column algo
+      div.column alg2
+      div.column algo3
+      div.column algo4
+      DrawCanvas
 </template>
 
-<script>
+<script lang="ts">
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import DrawCanvas from '@/components/DrawCanvas.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    DrawCanvas
   }
 }
 </script>
+
+<style lang="sass">
+  @import ../styles/variables
+  .column
+    color: white
+    background-color: $primary-colour1
+</style>
