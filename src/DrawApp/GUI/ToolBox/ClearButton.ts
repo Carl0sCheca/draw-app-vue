a377dd320662ccee106922fcfa15c303147eb515
Carlos Checa <carloschecaa@gmail.com>
2020-08-12 17:03:16 +0200

    bugfix

--- src/DrawApp/GUI/ToolBox/ClearButton.ts
+++ src/DrawApp/GUI/ToolBox/ClearButton.ts
@@ -1,6 +1,13 @@
 import { GUIElement } from '../GUIElement'
 
 export class ClearButton extends GUIElement {
+  public windowResize () {
+    this.position = {
+      x: this.drawApp.canvas.width - this.size.x,
+      y: 0
+    }
+  }
+
   public ui (): void {
     this.drawApp.ctx.drawImage(this.img, this.position.x, this.position.y, this.size.x, this.size.y)
   }
